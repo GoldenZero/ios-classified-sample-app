ac976e05b8a4844639d92000ad4cb5f00bbab38b
roula-misrabi <roula.misrabi@syrisoft.com>
2013-03-22 16:23:39 +0200

    Add Splash view files and set the view - header of nib added
    (By Noor)

---
+++ Bezaat/Bezaat/Main/View/Actions/SplashViewController.h
@@ -0,0 +1,14 @@
+//
+//  SplashViewController.h
+//  Bezaat
+//
+//  Created by Syrisoft on 3/22/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *splashView;
+
+@end
