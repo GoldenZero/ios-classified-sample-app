52ee00eaa18c34cf64cf6690f58e9cf862cfeb63
Noor Sarraj <noor.sarraj@syrisoft.com>
2013-03-24 14:36:32 +0200

    load countries and cities and set location view UI.

--- Bezaat/Bezaat/Main/View/Actions/ChooseLocationViewController.h
+++ Bezaat/Bezaat/Main/View/Actions/ChooseLocationViewController.h
@@ -3,28 +3,12 @@
 //  Bezaat
 //
 //  Created by Roula Misrabi on 3/3/13.
+//  Edited by Noor Alssarraj on 24/3/13.
 //  Copyright (c) 2013 Syrisoft. All rights reserved.
 //
 // This UI is displayed to the user the first time the application starts.
 // It is responsible for displaying countries and cities with map images as background to let the user choose from.
 
-/*
-#import <UIKit/UIKit.h>
-#import "DropDownList.h"
-#import "SignInViewController.h"
-#import "CountryLoader.h"
-
-@interface ChooseLocationViewController : UIViewController <DropDownListDelegate, CountryLoaderDelegate>
-
-#pragma mark - properties
-@property (weak, nonatomic) IBOutlet UIButton *nextBtn;
-@property (weak, nonatomic) IBOutlet UIImageView *backgroungImageView;
-
-#pragma mark - actions
-- (IBAction)nextBtnPressed:(id)sender;
-
-@end
-*/
 
 
 #import <UIKit/UIKit.h>
@@ -32,11 +16,12 @@
 #import "SignInViewController.h"
 #import "LocationManager.h"
 
-@interface ChooseLocationViewController : UIViewController <DropDownListDelegate>
+@interface ChooseLocationViewController : UIViewController <DropDownListDelegate,LocationManagerDelegate>
 
 #pragma mark - properties
-@property (weak, nonatomic) IBOutlet UIButton *nextBtn;
-@property (weak, nonatomic) IBOutlet UIImageView *backgroungImageView;
+
+@property (strong, nonatomic) IBOutlet UIButton *nextBtn;
+@property (strong, nonatomic) IBOutlet UIImageView *backgroungImageView;
 
 #pragma mark - actions
 - (IBAction)nextBtnPressed:(id)sender;
