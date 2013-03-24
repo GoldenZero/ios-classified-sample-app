a23bf9b9b05851696c760a5d0b5968f97a4a3815
roula-misrabi <roula.misrabi@syrisoft.com>
2013-03-24 09:00:04 +0200

    Added "CountryManager" class

---
+++ Bezaat/Bezaat/Main/Controller/Locations/CountryManager.m
@@ -0,0 +1,13 @@
+//
+//  CountryManager.m
+//  Bezaat
+//
+//  Created by Roula Misrabi on 3/24/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import "CountryManager.h"
+
+@implementation CountryManager
+
+@end
