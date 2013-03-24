d2bb905a5d37bb644ad4045b72244223a9ad1d0e
roula-misrabi <roula.misrabi@syrisoft.com>
2013-03-24 09:41:20 +0200

    Commit changes:
    - Added LocationManager class
    - Removed CountryManager class

---
+++ Bezaat/Bezaat/Main/Controller/Locations/LocationManager.h
@@ -0,0 +1,13 @@
+//
+//  LocationManager.h
+//  Bezaat
+//
+//  Created by Roula Misrabi on 3/24/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationManager : NSObject
+
+@end
---
+++ Bezaat/Bezaat/Main/Controller/Locations/LocationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocationManager.m
+//  Bezaat
+//
+//  Created by Roula Misrabi on 3/24/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
