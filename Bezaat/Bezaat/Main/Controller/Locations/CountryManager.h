d40f647f6faf868316552191049b56a64930556e
roula-misrabi <roula.misrabi@syrisoft.com>
2013-03-24 09:01:10 +0200

    Updated "CountryManager" class

--- Bezaat/Bezaat/Main/Controller/Locations/CountryManager.h
+++ Bezaat/Bezaat/Main/Controller/Locations/CountryManager.h
@@ -7,7 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AubadaLibrary/JSONParser.h>
+#import "Country.h"
+
+@protocol CountryManagerDelegate <NSObject>
+@required
+- (void) countriesDidFinishLoadingWithData:(NSArray*) resultArray;
+@end
+
 
 @interface CountryManager : NSObject
+
+#pragma mark - properties
+@property (strong, nonatomic) id <CountryManagerDelegate> delegate;
+
+#pragma mark - methods
+- (id) initWithDelegate:(id <CountryManagerDelegate>) del;
+
+// load countries & cities
+- (void) loadCountries;
+
+// get the default selected country index in countries result array
+- (NSUInteger) getDefaultSelectedCountryIndex;
+
+// get the default selected city index in cities result array
+- (NSUInteger) getDefaultSelectedCityIndexForCountry:(NSUInteger) countryID;
 
 @end
