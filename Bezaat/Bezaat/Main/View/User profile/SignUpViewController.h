4c04189e3ccdddc03e26a2d9f655fc157e2f9c12
Noor Sarraj <noor.sarraj@syrisoft.com>
2013-03-24 19:05:57 +0200

    set sign up UI view.

--- Bezaat/Bezaat/Main/View/User profile/SignUpViewController.h
+++ Bezaat/Bezaat/Main/View/User profile/SignUpViewController.h
@@ -3,16 +3,31 @@
 //  Bezaat
 //
 //  Created by Roula Misrabi on 3/3/13.
+//  Edited by Noor Alssarraj on 24/3/13.
 //  Copyright (c) 2013 Syrisoft. All rights reserved.
 //
 // This UI is displayed to add a new user profile.
 
 #import <UIKit/UIKit.h>
 
-@interface SignUpViewController : UIViewController
+@interface SignUpViewController : UIViewController<UITextViewDelegate>
 
 #pragma mark - properties
+@property (strong, nonatomic) IBOutlet UIToolbar *toolBar;
+@property (strong, nonatomic) IBOutlet UIImageView *titleView;
+@property (strong, nonatomic) IBOutlet UIButton *facebookButton;
+@property (strong, nonatomic) IBOutlet UIButton *twitterButton;
+@property (strong, nonatomic) IBOutlet UIButton *deleteButton;
+@property (strong, nonatomic) IBOutlet UIButton *saveButton;
+@property (strong, nonatomic) IBOutlet UIImageView *sepratorImage;
+@property (strong, nonatomic) IBOutlet UITextField *userText;
+@property (strong, nonatomic) IBOutlet UITextField *emailText;
+@property (strong, nonatomic) IBOutlet UITextField *passwordText;
+@property (strong, nonatomic) IBOutlet UITextField *confirmPasswordText;
 
 #pragma mark - actions
-
+- (IBAction)fbButtonPressed:(id)sender;
+- (IBAction)twButtonPressed:(id)sender;
+- (IBAction)deleteButtonPressed:(id)sender;
+- (IBAction)saveButtonPressed:(id)sender;
 @end
--- Bezaat/Bezaat/Main/View/User profile/SignUpViewController.m
+++ Bezaat/Bezaat/Main/View/User profile/SignUpViewController.m
@@ -3,6 +3,7 @@
 //  Bezaat
 //
 //  Created by Roula Misrabi on 3/3/13.
+//  Edited by Noor Alssarraj on 24/3/13.
 //  Copyright (c) 2013 Syrisoft. All rights reserved.
 //
 
@@ -26,6 +27,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self setBackgroundImages];
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -34,5 +36,50 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark -- Actions
 
+- (IBAction)fbButtonPressed:(id)sender {
+}
+
+- (IBAction)twButtonPressed:(id)sender {
+}
+
+- (IBAction)deleteButtonPressed:(id)sender {
+}
+
+- (IBAction)saveButtonPressed:(id)sender {
+}
+- (void)popSelf{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark -- custom methods
+- (void) setBackgroundImages{
+    [self.toolBar setBackgroundImage:[UIImage imageNamed:@"home_blueRectangle.png"] forToolbarPosition:0 barMetrics:UIBarMetricsDefault];
+    [self.facebookButton setBackgroundImage:[UIImage   imageNamed:@"SignUp_Facebook.png"] forState:UIControlStateNormal];
+    [self.twitterButton setBackgroundImage:[UIImage   imageNamed:@"SignUp_Twitter.png"] forState:UIControlStateNormal];
+    [self.saveButton setBackgroundImage:[UIImage   imageNamed:@"SignUp_rigester.png"] forState:UIControlStateNormal];
+    [self.deleteButton setBackgroundImage:[UIImage   imageNamed:@"SignUp_Clear.png"] forState:UIControlStateNormal];
+    UIImageView *background=[[UIImageView alloc] initWithFrame:CGRectMake(0,0,320, 480)];
+    background.image=[UIImage imageNamed:@"SignUp_Bottom_bg.png"];
+    [self.view addSubview:background];
+    [self.view sendSubviewToBack:background];
+    self.titleView.image=[UIImage imageNamed:@"SignUp_Top_BG.png"];
+    self.sepratorImage.image=[UIImage imageNamed:@"SignUp_Or_Slice.png"];
+    
+    UIImage * backBtnImg = [UIImage imageNamed:@"share_arrowButton.png"];
+    UIButton * customBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 40 , 40)];
+    [customBtn setImage:backBtnImg forState:UIControlStateNormal];
+    [customBtn addTarget:self action:@selector(popSelf) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem * button = [[UIBarButtonItem alloc] initWithCustomView:customBtn];
+    NSArray * items = [NSArray arrayWithObjects:button, nil];
+    [self.toolBar setItems:items];
+
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
 @end
