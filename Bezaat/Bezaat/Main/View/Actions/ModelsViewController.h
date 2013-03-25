bef536a0969cff8402db5dede68fdd279f9563ec
Aubada Taljo <aubada.taljo@syrisoft.com>
2013-03-25 11:13:58 +0200

    Added Brands & Models UI files

---
+++ Bezaat/Bezaat/Main/View/Actions/ModelsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModelsViewController.h
+//  Bezaat
+//
+//  Created by Aubada Taljo on 3/25/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModelsViewController : UIViewController
+
+@end
