7c9e933ef317a3ad5d234af31b2bb156b75ed78d
roula-misrabi <roula.misrabi@syrisoft.com>
2013-04-01 10:20:22 +0300

    Added twitter unofficial-sdk

---
+++ Bezaat/Bezaat/Supporting classes/Twitter Library/OAuth/OAuthConsumerCredentials.h
@@ -0,0 +1,12 @@
+/*
+ *  OAuthConsumerCredentials.h
+ *
+ *  Created by Jaanus Kase on 16.01.10.
+ *  Copyright 2010. All rights reserved.
+ *
+ */
+
+//#error Replace these with your own app credentials, then remove this error.
+
+#define OAUTH_CONSUMER_KEY @"A7gS8ZDjgPiX25VqMGl7uw"
+#define OAUTH_CONSUMER_SECRET @"y4HRxBufI2Pv3qbDUuq0C0zZVMbyK1pZQZBbQvk"
