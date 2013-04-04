c8c69f676a0aa13ba1735553da743da25ff339fc
roula-misrabi <roula.misrabi@syrisoft.com>
2013-04-04 14:09:42 +0300

    Added extra helper classes.

---
+++ Bezaat/Bezaat/Supporting classes/CustomError.h
@@ -0,0 +1,15 @@
+//
+//  CustomError.h
+//  Bezaat
+//
+//  Created by Roula Misrabi on 4/4/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomError : NSError
+
+@property (strong, nonatomic) NSString * descMessage;
+
+@end
---
+++ Bezaat/Bezaat/Supporting classes/CustomError.m
@@ -0,0 +1,19 @@
+//
+//  CustomError.m
+//  Bezaat
+//
+//  Created by Roula Misrabi on 4/4/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import "CustomError.h"
+
+@implementation CustomError
+@synthesize descMessage;
+
+- (NSString *) description {
+    
+    return descMessage;
+}
+
+@end
