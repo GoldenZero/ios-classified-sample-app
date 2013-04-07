118b6449d2c50a3bf64556de66ced4774a7d873d
roula-misrabi <roula.misrabi@syrisoft.com>
2013-04-07 23:11:23 +0300

    Added parsing ads data.

--- Bezaat/Bezaat/CarAds/Controller/CarAdsManager.m
+++ Bezaat/Bezaat/CarAds/Controller/CarAdsManager.m
@@ -1,4 +1,4 @@
-//
+ //
 //  CarAdsManager.m
 //  Bezaat
 //
@@ -9,6 +9,10 @@
 #import "CarAdsManager.h"
 
 #pragma mark - JSON Keys
+
+#define LISTING_STATUS_CODE_JKEY    @"StatusCode"
+#define LISTING_STATUS_MSG_JKEY     @"StatusMessage"
+#define LISTING_DATA_JKEY           @"Data"
 
 #define LISTING_ADD_ID_JKEY         @"AdID"
 #define LISTING_OWNER_ID_JKEY       @"OwnerID"
@@ -130,17 +134,74 @@ static NSString * internetMngrTempFileName = @"mngrTmp";
 
 - (void) manager:(BaseDataManager*)manager connectionDidFailWithError:(NSError*) error {
 
-    NSLog(@"Failure!! X(");
+    if (self.delegate)
+        [delegate adsDidFailLoadingWithError:error];
 }
 
 - (void) manager:(BaseDataManager*)manager connectionDidSucceedWithObjects:(NSData*) result {
-    NSLog(@"data received!");
+    
+    if (!result)
+    {
+        CustomError * error = [CustomError errorWithDomain:@"" code:-1 userInfo:nil];
+        [error setDescMessage:@"فشل اتحميل البيانات"];
+        
+        if (self.delegate)
+            [self.delegate adsDidFailLoadingWithError:error];
+    }
+    else
+    {
+        if (self.delegate)
+        {
+            NSArray * adsArray = [self createCarAdsArrayWithData:(NSArray *)result];
+            [delegate adsDidFinishLoadingWithData:adsArray];
+        }
+    }
 }
 
 #pragma mark - helper methods
 
 - (NSArray * ) createCarAdsArrayWithData:(NSArray *) data {
     
+    if ((data) && (data.count > 0))
+    {
+        NSDictionary * totalDict = [data objectAtIndex:0];
+        NSString * statusCodeString = [totalDict objectForKey:LISTING_STATUS_CODE_JKEY];
+        NSInteger statusCode = statusCodeString.integerValue;
+        
+        NSMutableArray * adsArray = [NSMutableArray new];
+        if (statusCode == 200)
+        {
+            NSArray * dataAdsArray = [totalDict objectForKey:LISTING_DATA_JKEY];
+            if ((dataAdsArray) && (dataAdsArray.count))
+            {
+                for (NSDictionary * adDict in dataAdsArray)
+                {
+                    CarAd * ad =
+                    [[CarAd alloc]
+                     initWithAdIDString:[adDict objectForKey:LISTING_ADD_ID_JKEY]
+                     ownerIDString:[adDict objectForKey:LISTING_OWNER_ID_JKEY]
+                     storeIDString:[adDict objectForKey:LISTING_STORE_ID_JKEY]
+                     isFeaturedString:[adDict objectForKey:LISTING_IS_FEATURED_JKEY]
+                     thumbnailURL:[adDict objectForKey:LISTING_THUMBNAIL_URL_JKEY]
+                     title:[adDict objectForKey:LISTING_TITLE_JKEY]
+                     priceString:[adDict objectForKey:LISTING_PRICE_JKEY]
+                     currencyString:[adDict objectForKey:LISTING_CURRENCY_JKEY]
+                     postedOnDateString:[adDict objectForKey:LISTING_POSTED_ON_JKEY]
+                     modelYearString:[adDict objectForKey:LISTING_MODEL_YEAR_JKEY]
+                     distanceRangeInKmString:[adDict objectForKey:LISTING_DISTANCE_KM_JKEY]
+                     viewCountString:[adDict objectForKey:LISTING_VIEW_COUNT_JKEY]
+                     isFavoriteString:[adDict objectForKey:LISTING_IS_FAVORITE_JKEY]
+                     storeName:[adDict objectForKey:LISTING_STORE_NAME_JKEY]
+                     storeLogoURL:[adDict objectForKey:LISTING_STORE_LOGO_URL_JKEY]
+                     ];
+                    
+                    [adsArray addObject:ad];
+                }
+            }
+        }
+        return adsArray;
+    }
+    return nil;
 }
 
 @end
--- Bezaat/Bezaat/main.m
+++ Bezaat/Bezaat/main.m
@@ -9,13 +9,10 @@
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
-#import "CarAdsManager.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        
-        [[CarAdsManager sharedInstance] loadCarAdsOfPage:1 forBrand:363 Model:208 InCity:13 WithDelegate:nil];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
