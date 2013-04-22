a745a644859181e7fde66fde5373a472475b368c
roula-misrabi <roula.misrabi@syrisoft.com>
2013-04-22 15:33:33 +0300

    Added new resource file for 'add new car'

---
+++ Bezaat/Bezaat/CarAds/Controller/StaticAttrsLoader.h
@@ -0,0 +1,13 @@
+//
+//  StaticAttrsLoader.h
+//  Bezaat
+//
+//  Created by Roula Misrabi on 4/22/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticAttrsLoader : NSObject
+
+@end
---
+++ Bezaat/Bezaat/CarAds/Controller/StaticAttrsLoader.m
@@ -0,0 +1,13 @@
+//
+//  StaticAttrsLoader.m
+//  Bezaat
+//
+//  Created by Roula Misrabi on 4/22/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import "StaticAttrsLoader.h"
+
+@implementation StaticAttrsLoader
+
+@end
