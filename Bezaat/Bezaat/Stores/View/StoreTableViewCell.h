d5d99c34ba314944d939fa7d648b9663f372ccbc
Alaa Al-Zaibak <alaaite@gmail.com>
2013-04-22 22:48:03 +0300

    + Add StoreTableViewCell files

---
+++ Bezaat/Bezaat/Stores/View/StoreTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  StoreTableViewCell.h
+//  Bezaat
+//
+//  Created by Alaa Al-Zaibak on 4/22/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoreTableViewCell : UITableViewCell
+
+@end
---
+++ Bezaat/Bezaat/Stores/View/StoreTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  StoreTableViewCell.m
+//  Bezaat
+//
+//  Created by Alaa Al-Zaibak on 4/22/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import "StoreTableViewCell.h"
+
+@implementation StoreTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
