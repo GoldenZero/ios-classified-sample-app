8992066c752238f4e27797b6e69b94ba33d2801a
roula-misrabi <roula.misrabi@syrisoft.com>
2013-04-23 17:29:26 +0300

    Added featuring manager class

---
+++ Bezaat/Bezaat/CarAds/Controller/FeaturingManager.h
@@ -0,0 +1,15 @@
+//
+//  FeaturingManager.h
+//  Bezaat
+//
+//  Created by Roula Misrabi on 4/23/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PricingOption.h"
+#import "FeatutreOrder.h"
+
+@interface FeaturingManager : NSObject
+
+@end
---
+++ Bezaat/Bezaat/CarAds/Controller/FeaturingManager.m
@@ -0,0 +1,23 @@
+//
+//  FeaturingManager.m
+//  Bezaat
+//
+//  Created by Roula Misrabi on 4/23/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import "FeaturingManager.h"
+
+//pricing options
+#define PRICING_OPTIONS_PRICING_ID_JKEY         @"PricingID"
+#define PRICING_OPTIONS_COUNTRY_ID_JKEY         @"CountryID"
+#define PRICING_OPTIONS_CATEGORY_ID_JKEY        @"CategoryID"
+#define PRICING_OPTIONS_AD_PERIOD_DAYS_JKEY     @"AdPeriodDays"
+#define PRICING_OPTIONS_PRICE_JKEY              @"Price"
+#define PRICING_OPTIONS_CURRENCY_ID_JKEY        @"CurrencyID"
+#define PRICING_OPTIONS_CURRENCY_NAME_JKEY      @"CurrencyName"
+#define PRICING_OPTIONS_CURRENCY_NAME_EN_JKEY   @"CurrencyNameEn"
+
+@implementation FeaturingManager
+
+@end
