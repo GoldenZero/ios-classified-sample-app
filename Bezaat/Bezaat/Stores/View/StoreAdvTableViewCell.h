0dd5518b021bca88e690cb1db2eb7b869c8a8ab7
Alaa Al-Zaibak <alaaite@gmail.com>
2013-04-28 18:53:48 +0300

    Add Custom UITableCell for store adv

---
+++ Bezaat/Bezaat/Stores/View/StoreAdvTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  StoreAdvTableViewCell.h
+//  Bezaat
+//
+//  Created by Alaa Al-Zaibak on 4/28/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoreAdvTableViewCell : UITableViewCell
+
+@end
---
+++ Bezaat/Bezaat/Stores/View/StoreAdvTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  StoreAdvTableViewCell.m
+//  Bezaat
+//
+//  Created by Alaa Al-Zaibak on 4/28/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import "StoreAdvTableViewCell.h"
+
+@implementation StoreAdvTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
