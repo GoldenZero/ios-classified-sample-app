c2c05ad10bf48127d22de19b2f5b2da1c5c36c42
Noor Sarraj <noor.sarraj@syrisoft.com>
2013-07-04 18:46:57 +0300

    Adding cars ads in gallerey UI
    without backend.

---
+++ Bezaat/Bezaat/Main/View/Actions/CarsInGalleryViewController.h
@@ -0,0 +1,13 @@
+//
+//  CarsInGalleryViewController.h
+//  Bezaat
+//
+//  Created by Syrisoft on 7/4/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarsInGalleryViewController : UIViewController
+
+@end
---
+++ Bezaat/Bezaat/Main/View/Actions/CarsInGalleryViewController.m
@@ -0,0 +1,38 @@
+//
+//  CarsInGalleryViewController.m
+//  Bezaat
+//
+//  Created by Syrisoft on 7/4/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import "CarsInGalleryViewController.h"
+
+@interface CarsInGalleryViewController ()
+
+@end
+
+@implementation CarsInGalleryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
