7897a76162bbc2324b3a2a12a52b1ff5db40b518
Noor Sarraj <noor.sarraj@syrisoft.com>
2013-07-09 09:57:01 +0300

    Add custom cell of cars in gallery.

---
+++ Bezaat/Bezaat/Main/View/Actions/carInGalleryCell.h
@@ -0,0 +1,26 @@
+//
+//  carInGalleryCell.h
+//  Bezaat
+//
+//  Created by Syrisoft on 7/7/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface carInGalleryCell : UITableViewCell
+
+#pragma mark - properties
+@property (strong, nonatomic) IBOutlet UIImageView *cellBackgoundImage;
+@property (strong, nonatomic) IBOutlet SSLabel *detailsLabel;
+@property (strong, nonatomic) IBOutlet SSLabel *carPriceLabel;
+@property (strong, nonatomic) IBOutlet UILabel *addTimeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *yearLabel;
+@property (strong, nonatomic) IBOutlet UILabel *watchingCountsLabel;
+@property (strong, nonatomic) IBOutlet UILabel *carMileageLabel;
+@property (strong, nonatomic) IBOutlet UIButton *favoriteButton;
+@property (strong, nonatomic) IBOutlet UIImageView *distingushingImage;
+@property (strong, nonatomic) IBOutlet UIImageView *countOfViewsTinyImg;
+@property (strong, nonatomic) IBOutlet UIImageView *carImage;
+
+@end
